0e272788eaa1517b4421dc0c963041110bfaf5e8
akumari <akumari@redhat.com>
2024-11-27 00:19:03 +0530

    migrate pgsql installation from rpm to container-based

--- tests/postgresql_test.py
+++ tests/postgresql_test.py
@@ -1,3 +1,6 @@
+import re
+
+
 def test_postgresql_service(server):
     postgresql = server.service("postgresql")
     assert postgresql.is_running
@@ -7,3 +10,22 @@ def test_postgresql_service(server):
 def test_postgresql_port(server):
     postgresql = server.addr("localhost")
     assert postgresql.port("5432").is_reachable
+
+
+def test_postgresql_databases(server):
+    result = server.run("podman exec postgresql psql -U postgres -c '\\l'")
+    assert "foreman" in result.stdout
+    assert "candlepin" in result.stdout
+    assert "pulp" in result.stdout
+
+
+def test_postgresql_users(server):
+    result = server.run("podman exec postgresql psql -U postgres -c '\\du'")
+    assert "foreman" in result.stdout
+    assert "candlepin" in result.stdout
+    assert "pulp" in result.stdout
+
+
+def test_postgresql_hba_config(server):
+    result = server.run("podman exec postgresql cat /var/lib/pgsql/data/pg_hba.conf")
+    assert re.search(r"host\s+all\s+all\s+127\.0\.0\.1/32\s+md5", result.stdout)
