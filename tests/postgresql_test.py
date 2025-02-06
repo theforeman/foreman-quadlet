c9b7dcae8483e4b040e0728aad69081192804016
akumari <akumari@redhat.com>
2025-02-06 15:32:59 +0530

    add .container config

--- tests/postgresql_test.py
+++ tests/postgresql_test.py
@@ -6,7 +6,6 @@ def test_postgresql_service(server):
     assert postgresql.is_running
     assert postgresql.is_enabled
 
-
 def test_postgresql_port(server):
     postgresql = server.addr("localhost")
     assert postgresql.port("5432").is_reachable
